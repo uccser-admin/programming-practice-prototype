1bdba1993ff2574bf64d2ff7e4f8bfdf55d14f07
JackMorganNZ <jackmorgannz@gmail.com>
2019-06-04 11:31:29 +1200

    Test automated deployment to production

---
+++ infrastructure/prod-deploy/update-content.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source ./infrastructure/prod-deploy/load-prod-deploy-config-envs.sh
+
+# Updates the database for the development system
+cp ./infrastructure/cloud-sql-proxy/docker-compose.yml ./docker-compose.yml
+
+# Decrypt secret files archive that contain credentials.
+./infrastructure/prod-deploy/decrypt-prod-secrets.sh
+
+# Load environment variables.
+source ./codewof/load-prod-envs.sh
+
+# Start the system and run the migrate and collect_static system commands.
+./dev start
+./dev migrate
+./dev load_questions
